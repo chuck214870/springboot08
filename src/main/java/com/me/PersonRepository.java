cc836a0fc78563dcad562c247a2cb8cbb122b245
chuck.white <cwhite_2@yahoo.com>
2017-08-07 21:11:35 -0400

    Initial commit

---
+++ src/main/java/com/me/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.me;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by oracle on 8/7/17.
+ */
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
